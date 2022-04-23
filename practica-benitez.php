6c7df552c19e206b86237c07b19c45404f0d0f39
pilarbenitez1 <104272773+pilarbenitez1@users.noreply.github.com>
2022-04-23 20:03:08 +0200

    Add files via upload

---
+++ practica-benitez.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Función que nos permite averiguar la suma de
+* dos parámetros introducidos
+*@author Pilar Benítez Serrano <pilarbenitezserrano@gmail.com>
+*@version 0.0 Versión inicial del ejercicio
+*@internal Este ejercicio es de prueba para la entrega 5
+*@param int $x Primer parámetro
+*@param int $y Segundo parámetro
+*@return int Resultado de la suma
+*/
+function suma(int $x, int $y){
+$z=$x + $y;
+return $z;
+}
+echo "5 + 10 =".suma(5,10)."<br>";
+echo "7 + 2 =".suma(7, 2)."<br>";
+
+/**
+* Función que nos permite averiguar la resta de
+* dos parámetros introducidos
+*@author Pilar Benítez Serrano <pilarbenitezserrano@gmail.com>
+*@version 0.1 Versión inicial del ejercicio
+*@internal Este ejercicio es de prueba para la entrega 5
+*@param int $a Primer parámetro
+*@param int $b Segundo parámetro
+*@return int Resultado de la suma
+*/
+
+function resta(int $a, int $b){
+$c=$a-$b;
+return $c;
+}
+echo "5 - 10 =".resta(5, 10)."<br>";
+echo "7 - 2 =".resta(7, 2)."<br>";
+
